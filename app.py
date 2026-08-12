8dbf047c6a7b30489bd0d9b15844a242decca6bf
PSMEDISELLER <dhalihimadri1998@gmail.com>
2026-08-13 00:08:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -214,9 +214,7 @@ if choice == "📍 নতুন লোকেশন এড করুন":
 
     if st.button("লোকেশন সেভ করুন"):
         if party_name and lat != 0.0 and lon != 0.0:
-            c.execute("INSERT INTO locations (party_name, address, party_phone, lat, lon) VALUES (?, ?, ?, ?, ?)", 
-          (party_name, address, party_phone, lat, lon))
-                      (party_name, address, party_phone, lat, lon))       
+           c.execute("INSERT INTO locations (party_name, address, party_phone, lat, lon) VALUES (?, ?, ?, ?, ?)", (party_name, address, party_phone, lat, lon))      
             conn.commit()
             st.success(f"'{party_name}' এর লোকেশন সফলভাবে সেভ হয়েছে!")
         else:
