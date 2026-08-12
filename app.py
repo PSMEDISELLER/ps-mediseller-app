35be24c8fdbaa45ce7954706ac82e2881c565083
PSMEDISELLER <dhalihimadri1998@gmail.com>
2026-08-13 01:45:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -333,7 +333,7 @@ st.subheader("ডেলিভারি ও রুট প্ল্যানার
 st.sidebar.write("---")
 st.sidebar.subheader("📍 GPS")
 
-    st.write("📍 **লাইভ লোকেশন:**")
+st.write("📍 **লাইভ লোকেশন:**")
 
     st.info("নিচের বাটনে চাপলে আপনার ব্রাউজার GPS permission চাইবে।")
 
